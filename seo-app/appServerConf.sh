feddafa520a7567dc96f4c980e929354c2adb218
valerii <valerii15298@gmail.com>
2020-11-16 21:09:17 +0200

    init

--- seo-app/appServerConf.sh
+++ seo-app/appServerConf.sh
@@ -20,15 +20,12 @@ apt install -y docker-ce docker-ce-cli containerd.io
 curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 sudo apt install -y nodejs gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget libgbm-dev
 
-#to restart app after changes run: pm2 restart testUrl.js
-# pm2 logs
 #copy files to app dir
 cd $rootdir && git clone https://github.com/valerii15298/seo.git app && cd app/seo-app && npm init -y && npm i node-fetch express puppeteer googleapis && npm install -g pm2
 
 # Configure Screaming frog
 chmod +x $rootdir/app/install-sf.sh && source $rootdir/app/install-sf.sh
 echo "unset DISPLAY" >>~/.bashrc
-
 
 #configure docker private instance webpagetest server and agent
 docker build -t local-wptserver "$rootdir/app/webpagetest/server"
@@ -42,6 +39,11 @@ docker run -d -p 4001:80 \
   local-wptagent
 
 # start application
-pm2 start $rootdir/app/seo-app/testUrl.js --watch
-pm2 startup systemd # exec command that it shows
+pm2 start $rootdir/app/seo-app/testUrl.js --watch # start app and restart it automatically if source app changes
+pm2 startup systemd                               # exec command that it shows
+#pm2 restart testUrl.js # to restart app after changes run:
+# pm2 logs # show logs
+
+# update, upgrade and restart
+apt update && apt upgrade -y
 reboot
