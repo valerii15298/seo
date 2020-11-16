592375c2c443ae36a1b297d272bf7ce6123c4396
valerii <valerii15298@gmail.com>
2020-11-16 14:11:52 +0200

    AA

--- screaming-script.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p "screaming-frog/$1"
-
-screamingfrogseospider --crawl "$1" --overwrite --headless --output-folder "screaming-frog/$1" --export-tabs "Internal:All,External:All"
-
-awk -F '","'  'BEGIN {OFS=","} { if (toupper($3) != "200")  print }' "screaming-frog/$1/internal_all.csv" | awk -F"," '{print $1}' > "screaming-frog/$1/bad_internal.csv"
-
-awk -F '","'  'BEGIN {OFS=","} { if (toupper($3) != "200")  print }' "screaming-frog/$1/external_all.csv" | awk -F"," '{print $1}' > "screaming-frog/$1/bad_external.csv"
--- webPageTestServerConf.sh
+++
@@ -1,7 +0,0 @@
-# Ubuntu 18.04 LTS. server for webPageTest
-# see url https://github.com/WPO-Foundation/webpagetest-docs/blob/master/user/Private%20Instances/gce_server.md
-# then https://github.com/WPO-Foundation/webpagetest-docs/blob/master/user/Private%20Instances/gce_agents.md
-
-# then add auth in index.php in app , with customtoken
-
-# That`s all !
